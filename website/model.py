e3c0c8a8a9b4cb0555f2bad6fb55e6b7c25e067b
Tran Minh Triet <tranminhtriet0@gmail.com>
2023-12-02 21:06:58 +0700

    clean up

--- website/model.py
+++ website/model.py
@@ -19,4 +19,32 @@ class Patient(db.Model, UserMixin):
         return self.Code
     
 
-    
+def add_patient():
+    if request.method == 'POST':
+        # Get form data
+        last_name = request.form.get('lastName')
+        first_name = request.form.get('firstName')
+        phone_number = request.form.get('phoneNumber')
+        address = request.form.get('address')
+        date_of_birth = request.form.get('dateOfBirth')
+        gender = request.form.get('gender')
+        doctor_assigned = request.form.get('doctorAssigned')
+        medicine = request.form.get('medicine')
+
+        # Create a new patient instance
+        new_patient = Patient(
+            Last_Name=last_name,
+            First_Name=first_name,
+            Phone_Number=phone_number,
+            Address=address,
+            Date_of_Birth=date_of_birth,
+            Gender=gender,
+            Doctor_Assigned=doctor_assigned,
+            Medicine=medicine
+        )
+
+        # Add the patient to the database
+        db.session.add(new_patient)
+        db.session.commit()
+
+        return "Patient added successfully"
